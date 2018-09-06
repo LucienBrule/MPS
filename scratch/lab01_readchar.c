30127332e32fb2679002fbba81a3cc90e500c563
Ryan Rao <raor2@rpi.edu>
2018-09-06 19:09:02 -0400

    initial

--- scratch/lab01_readchar.c
+++ scratch/lab01_readchar.c
@@ -3,9 +3,15 @@
 int main(){
 	printf("### LAB 001 ###\n");
 	char key_pressed = 0;
-	while(key_pressed != 0x41){
+	while(key_pressed != 0x1B){
 		key_pressed = getchar();
+
+
 		if(key_pressed != 0x0A)
 			printf("You pressed: %x ... %c\n", key_pressed, key_pressed);
+
+		
 	}
+
+
 }

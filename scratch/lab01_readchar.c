f9b16c7b1f2da884d5dfe17c73bba76cc5c9c5e2
Lucien Brule <lucien@lucienbrule.com>
2018-09-10 18:50:05 -0400

    change text color

--- scratch/lab01_readchar.c
+++ scratch/lab01_readchar.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
+
+#define TEXT_ESCAPE "\033[0;33m\033[44m\n"
 
 int main(){
-	printf("### LAB 001 ###\n");
+	#ifdef TEXT_ESCAPE
+	 printf(TEXT_ESCAPE);
+	#endif
+
+	//python -c 'print((80 - len("PRESS <ESC> OR <CTRL> + [ TO QUIT")) * " ")'
+	printf("                                               PRESS <ESC> OR <CTRL> + [ TO QUIT");
+	printf(" \n \n \n \n \n \n");
 	char key_pressed = 0;
 	while(key_pressed != 0x1B){
 		key_pressed = getchar();
-
-
-		if(key_pressed != 0x0A)
-			printf("You pressed: %x ... %c\n", key_pressed, key_pressed);
-
-		
+		if(isprint(key_pressed)){
+			printf("The keyboard character is %c\n", key_pressed);
+		}
+		else if(key_pressed == 0x0A){
+			// Do nothing
+		}else{
+			printf("The keyboard character $%x is 'not printable'\a\n", key_pressed);
+		}
 	}
-
-
+	printf("Exiting");
 }

ba9ad614654c5f93a7a1d1064c66528579f6a374
Lucien Brule <lucien@lucienbrule.com>
2018-10-18 18:29:45 -0400

    add task 1, ADC conversion

--- LAB-04_Template/src/Lab04_adc.c
+++ LAB-04_Template/src/Lab04_adc.c
@@ -5,6 +5,9 @@
 //
 
 #include "init.h"
+void configureADC();
+
+ADC_HandleTypeDef ADCHandle;
 
 // Main Execution Loop
 int main(void)
@@ -12,32 +15,47 @@ int main(void)
 	//Initialize the system
 	Sys_Init();
 	configureADC();
+	HAL_ADC_Start(&ADCHandle);
+	printf("\r\n============ Starting LAB 04 ===============\r\n");
 
-	// Code goes here
+	for(;;){
+		uint16_t value = 0x00;
+		float voltage;
+		HAL_ADC_Start(&ADCHandle);
+		HAL_ADC_PollForConversion(&ADCHandle,100);
+		value = HAL_ADC_GetValue(&ADCHandle);
+		voltage =(double) (( (double) value* (double) 3300) / (double) 4095) / (double)1000;
+		printf("ADC: %x %u VOLTS: %f \r\n",value,value,(double) voltage);
+		HAL_Delay(1000);
+	}
 }
 
 void configureADC()
 {
 	// Enable the ADC Clock.
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+	__HAL_RCC_ADC1_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
-	HAL_ADC_Init(...); // Initialize the ADC
+	//Configure ADC1
+	ADCHandle.Instance = ADC1;
+	ADCHandle.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+	ADCHandle.Init.Resolution = ADC_RESOLUTION12b;
+	ADCHandle.Init.ContinuousConvMode = DISABLE;
+	ADCHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	ADCHandle.Init.NbrOfConversion = 0;
+	ADCHandle.Init.NbrOfDiscConversion = 1;
+	ADCHandle.Init.DiscontinuousConvMode = ENABLE;
+	ADCHandle.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+	HAL_ADC_Init(&ADCHandle); // Initialize the ADC
 
-	/* Available sampling times:
-
-		ADC_SAMPLETIME_3CYCLES
-	  ADC_SAMPLETIME_15CYCLES
-		ADC_SAMPLETIME_28CYCLES
-		ADC_SAMPLETIME_56CYCLES
-		ADC_SAMPLETIME_84CYCLES
-		ADC_SAMPLETIME_112CYCLES
-		ADC_SAMPLETIME_144CYCLES
-		ADC_SAMPLETIME_480CYCLES
-
-	*/
 
 	// Configure the ADC channel
-
-	HAL_ADC_ConfigChannel(...,...);
+	ADC_ChannelConfTypeDef ADC_Channel_InitStruct;
+	ADC_Channel_InitStruct.Channel = ADC_CHANNEL_6;
+	ADC_Channel_InitStruct.Rank = 1;
+	ADC_Channel_InitStruct.SamplingTime = ADC_SAMPLETIME_112CYCLES;
+	HAL_ADC_ConfigChannel(&ADCHandle,&ADC_Channel_InitStruct);
 }
 
 
@@ -45,5 +63,12 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 {
 
 // GPIO init
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.Pin = GPIO_PIN_6;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOA,&GPIO_InitStruct);
+
 
 }

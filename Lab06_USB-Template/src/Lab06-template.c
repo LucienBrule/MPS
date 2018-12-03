49439ce6b1f51124dec9fa9ab6950b06d48b7b8f
Lucien Brule <lucien@lucienbrule.com>
2018-12-03 11:16:24 -0500

    update lab 6

--- Lab06_USB-Template/src/Lab06-template.c
+++ Lab06_USB-Template/src/Lab06-template.c
@@ -14,6 +14,8 @@
 USBH_HandleTypeDef husbh;
 void USBH_UserProcess(USBH_HandleTypeDef *, uint8_t);
 uint8_t isMouseConnected = 0;
+uint8_t isMouseActive = 0;
+char data_ready = 0;
 
 HID_MOUSE_Info_TypeDef* mouse_info;
 int main(void) {
@@ -32,54 +34,25 @@ int main(void) {
 	USBH_Start(&husbh);
 
 	printf("Press any key to start\r\n");
-
 	char wait = 0;
-	while (wait == 0) {
-		wait = getchar();
-	}
+//	wait = getchar();
 	printf("HOST ID: %x\r\n", husbh.id);
-//	printf("TIMER: %x\r\n", husbh.Timer);
-//	printf("TIMER: %x\r\n", husbh.);
-	HAL_Delay(100);
 	while (1) {
 		USBH_Process(&husbh);
-//		printf("connected : %x", husbh.device.is_connected);
-//		printf("\r\n");
-//		HAL_Delay(1000);
-		// Other stuff
-		if (isMouseConnected) {
-			printf("Mouse is connected\r\n");
-			if (mouse_info != NULL)
-				printf("[x] %x [y] %x\r\n", (int) mouse_info->x,
-						(int) mouse_info->y);
-			else
-				printf("Return value was NULL\r\n");
+		//printf("Is Mouse Active: %x\r", isMouseActive);
+		if(data_ready && isMouseActive){
+			printf("here");fflush(stdout);
+			//mouse_info = USBH_HID_GetMouseInfo(&husbh);
 		}
-
 	}
 }
 
 void USBH_UserProcess(USBH_HandleTypeDef *phost, uint8_t id) {
-	// ...
-	//printf("This happened!\r\n");
-	printf("[INFO] ID is: %x\r\n", id);
-	if (id == HOST_USER_CONNECTION) {
-
-		printf("[INFO] Mouse was connected\r\n");
-	} else if (id == HOST_USER_CLASS_ACTIVE) {
-		printf("Mouse is active\r\n");
-
-//		printf("[x] %x [y] %x\r\n",mouse_info->x, mouse_info->y);
-	} else if (id == HOST_USER_DISCONNECTION) {
-		printf("[INFO] Mouse was disconnected\r\n");
-
-	}
+	if (id == HOST_USER_CLASS_ACTIVE)
+			isMouseActive = 1;
 }
-
 
 // Interrupts and Callbacks...
 void USBH_HID_EventCallback(USBH_HandleTypeDef *phost){
-	//printf("This happened\r\n");
-	mouse_info = USBH_HID_GetMouseInfo(&husbh);
-
+	data_ready = 1;
 }
